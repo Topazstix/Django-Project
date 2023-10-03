8dcf91e21c4dac40c05dee08b6521099c90f6703
Topazstix <r.drone@protonmail.ch>
2023-10-03 10:15:46 -0600

    Add windows venv environment and set env vars for both venvs

---
+++ .virtualenvs/djvenv-linux/etc/activate.d/set_env_vars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This will automatically load specified environment variables for the given
+# Conda env
+
+# Set Django's settings module
+export DJANGO_SETTINGS_MODULE='django_project.settings'
---
+++ .virtualenvs/djvenv-linux/etc/deactivate.d/unset_env_vars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This will automatically load specified environment variables for the given
+# Conda env
+
+# Set Django's settings module
+unset DJANGO_SETTINGS_MODULE
